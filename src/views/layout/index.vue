93283b0fc9a92f83bd9c64a7fc9804d1e1ffc04e
jiaming <743192023@qq.com>
2018-11-07 17:58:50 +0800

    some config

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,13 +1,22 @@
 <template>
-  <div id="home">12312321</div>
+  <div id="home">
+    123123123213123asdasdasdasdasASDSADSAD哈哈哈明天是个好天气
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Home',
+  data () {
+    return {}
+  }
 }
 </script>
 
-<style>
+<style lang="less">
+@import url('../../assets/style/index.less');
 
+#home {
+  background-color: @bc-color;
+}
 </style>
