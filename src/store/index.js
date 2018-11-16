aa38de651fcbfe092bc31c25e4b32a4d8b29dadf
jiaming <743192023@qq.com>
2018-11-16 16:39:45 +0800

    add songAudio state

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    musicAudio: ''
   },
   mutations: {
-
+    setMusicAudio (state, audio) {
+      state.musicAudio = audio
+    }
   },
   actions: {
 
