715f051dc017a005f40b761dfd375ff08e87e761
jiaming <743192023@qq.com>
2018-11-21 18:33:06 +0800

    add debounce fun

--- src/plugins/methodsExtend.js
+++ src/plugins/methodsExtend.js
@@ -8,21 +8,26 @@ export function deepClone (object) {
   return parse(stringify(object))
 }
 
-export function gameTimerFilter (timer) {
-  const { trunc } = Math
-
-  const minutes = trunc(timer / 60).toString().padStart(2, '0')
-  const seconds = (timer % 60).toString().padStart(2, '0')
-
-  return `${minutes}:${seconds}`
-}
-
 export function deleteArrayAllItems (arrays) {
   arrays.forEach(element => element.splice(0, element.length))
 }
 
+export function debounce (delay, callback) {
+  let lastTime
+
+  return function () {
+    clearTimeout(lastTime)
+
+    const [that, args] = [this, arguments]
+
+    lastTime = setTimeout(() => {
+      callback.apply(that, args)
+    }, delay)
+  }
+}
+
 export default function (Vue) {
   Vue.prototype.deepClone = deepClone
-  Vue.prototype.gameTimerFilter = gameTimerFilter
   Vue.prototype.deleteArrayAllItems = deleteArrayAllItems
+  Vue.prototype.debounce = debounce
 }
