ea8343a950531213d15a46be8975d3040584db07
jiaming <743192023@qq.com>
2018-11-23 16:13:13 +0800

    optmization

--- src/views/gallery/album.vue
+++ src/views/gallery/album.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="album">
-    <photo-cover :imgs="albums" :column="3" width="400" />
+    <photo-cover :imgs="albums" :column="3" width="400" @scroll="setFadeHeader" @click="doZoom" :class="{fade: zoomIndex !== false}" />
+
+    <div class="zoom" v-if="zoomIndex !== false">
+      <div class="zoom-img-container">
+        <img :src="albums[zoomIndex].src">
+        <i :class="`jm-${autoPlayStatus ? 'pause' : 'play'}`" @click="doAutoPlay" />
+      </div>
+
+      <i class="jm-left" @click="zoomIndex - 1 < 0 ? zoomIndex = albums.length - 1 : zoomIndex--" />
+      <i class="jm-right" @click="zoomIndex + 1 === albums.length ? zoomIndex = 0 : zoomIndex++" />
+      <i class="jm-fork" @click="zoomIndex = autoPlayStatus = false" />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
   name: 'Album',
   props: ['album'],
   data () {
     return {
+      // all albums
       albums: [
         {
           src: '/API/assets/temp/school/1.jpg'
@@ -50,13 +64,52 @@ export default {
         {
           src: '/API/assets/temp/school/13.jpg'
         }
-      ]
+      ],
+      // zoom img index
+      zoomIndex: false,
+      // auto play status
+      autoPlayStatus: false,
+      // auto play time frame
+      autoPlayTime: 3000
     }
   },
   methods: {
+    /**
+     * @description             zoom img
+     * @return     {undefined}  no return
+     */
+    doZoom (index) {
+      this.zoomIndex = index
+    },
+    /**
+     * @description             do auto play
+     * @return     {undefined}  no return
+     */
+    doAutoPlay () {
+      const { autoPlayStatus, autoPlay } = this
+
+      this.autoPlayStatus = !autoPlayStatus
+
+      if (this.autoPlayStatus) setTimeout(autoPlay, 1000)
+    },
+    /**
+     * @description             auto play
+     * @return     {undefined}  no return
+     */
+    autoPlay () {
+      const { albums: { length: albumsNum }, zoomIndex, autoPlay, autoPlayStatus, autoPlayTime } = this
+
+      if (!autoPlayStatus) return
+
+      const nextZoomIndex = zoomIndex + 1
+
+      this.zoomIndex = (nextZoomIndex === albumsNum ? 0 : nextZoomIndex)
+
+      setTimeout(autoPlay, autoPlayTime)
+    },
+    ...mapMutations(['setFadeHeader'])
   },
   created () {
-    console.error(this.album)
   }
 }
 </script>
@@ -65,7 +118,94 @@ export default {
 @import url('../../assets/style/index.less');
 
 #album {
+  position: relative;
   width: 100%;
   height: 100%;
+
+  .fade {
+    opacity: 0;
+  }
+
+  .photo-cover {
+    transition: all 0.3s;
+  }
+
+  .zoom {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0px;
+    left: 0px;
+
+    .zoom-img-container {
+      position: absolute;
+      top: 10%;
+      left: 50%;
+      height: 80%;
+      transform: translateX(-50%);
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      border-radius: 15px;
+
+      &:hover {
+        .jm-play, .jm-pause {
+          visibility: visible;
+          opacity: 1;
+        }
+      }
+
+      img {
+        height: 100%;
+        width: auto;
+        box-sizing: border-box;
+        padding: 10px;
+        border-radius: 15px;
+        .SBS(fade(@BSC, 60));
+      }
+    }
+
+    .jm-left, .jm-right, .jm-fork, .jm-play, .jm-pause {
+      position: absolute;
+      cursor: pointer;
+      color: transparent;
+      .STS(@TC);
+
+      &:active {
+        color: @TC;
+      }
+    }
+
+    .jm-left, .jm-right {
+      top: 50%;
+      transform: translateY(-50%);
+      font-size: 100px;
+    }
+
+    .jm-left {
+      left: 20px;
+    }
+
+    .jm-right {
+      right: 20px;
+    }
+
+    .jm-fork {
+      top: 100px;
+      right: 40px;
+      font-size: 60px;
+    }
+
+    .jm-play, .jm-pause {
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 9;
+      font-size: 100px;
+      opacity: 0;
+      visibility: hidden;
+      transition: all 0.3s;
+    }
+  }
 }
 </style>
--- src/views/gallery/contents.vue
+++ src/views/gallery/contents.vue
@@ -1,24 +1,12 @@
 <template>
   <div id="gallery-contents">
-    <div :class="`gallery-scroll-container ${contents.length <= 4 && 'less-four'}`" ref="gallery-scroll-container" @scroll="calcVisibleCoverDebounce">
-      <div class="column" v-for="column in columns" :key="column">
-
-        <div class="album" v-for="(album, index) in contents" :key="index" v-if="index % 4 === column">
-          <img :src="album.coverSrc" />
-          <div class="info" @click="$router.push(`/home/gallery/${album.name}`)">
-            <div class="info-container">
-              <div class="name">{{ album.name }}</div>
-              <div class="describe">{{ album.describe }}</div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
+    <photo-cover :imgs="contents" :column="4" width="300" info="true" @click="turnToAlbum" @scroll="setFadeHeader" />
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
   name: 'GalleryContents',
   data () {
@@ -26,122 +14,71 @@ export default {
       // album contents
       contents: [
         {
-          name: 'Album1',
-          coverSrc: '/API/assets/temp/pcontents/1.jpg',
+          src: '/API/assets/temp/pcontents/1.jpg',
+          title: 'Album1',
           describe: 'This is album 1'
         },
         {
-          name: 'Album2',
-          coverSrc: '/API/assets/temp/pcontents/2.jpg',
+          src: '/API/assets/temp/pcontents/2.jpg',
+          title: 'Album2',
           describe: 'This is album 2'
         },
         {
-          name: 'Album3',
-          coverSrc: '/API/assets/temp/pcontents/3.jpg',
+          src: '/API/assets/temp/pcontents/3.jpg',
+          title: 'Album3',
           describe: 'This is album 3'
         },
         {
-          name: 'Album4',
-          coverSrc: '/API/assets/temp/pcontents/4.jpg',
+          src: '/API/assets/temp/pcontents/4.jpg',
+          title: 'Album4',
           describe: 'This is album 4'
         },
         {
-          name: 'Album5',
-          coverSrc: '/API/assets/temp/pcontents/5.jpg',
+          src: '/API/assets/temp/pcontents/5.jpg',
+          title: 'Album5',
           describe: 'This is album 5'
         },
         {
-          name: 'Album6',
-          coverSrc: '/API/assets/temp/pcontents/6.jpg',
+          src: '/API/assets/temp/pcontents/6.jpg',
+          title: 'Album6',
           describe: 'This is album 6'
         },
         {
-          name: 'Album7',
-          coverSrc: '/API/assets/temp/pcontents/7.jpg',
+          src: '/API/assets/temp/pcontents/7.jpg',
+          title: 'Album7',
           describe: 'This is album 7'
         },
         {
-          name: 'Album8',
-          coverSrc: '/API/assets/temp/pcontents/8.jpg',
+          src: '/API/assets/temp/pcontents/8.jpg',
+          title: 'Album8',
           describe: 'This is album 8'
         },
         {
-          name: 'Album9',
-          coverSrc: '/API/assets/temp/pcontents/9.jpg',
+          src: '/API/assets/temp/pcontents/9.jpg',
+          title: 'Album9',
           describe: 'This is album 9'
         },
         {
-          name: 'Album10',
-          coverSrc: '/API/assets/temp/pcontents/10.jpg',
+          title: 'Album10',
+          src: '/API/assets/temp/pcontents/10.jpg',
           describe: 'This is album 10'
         }
-      ],
-      // column
-      columns: [],
-      // calc visible cover debounce fun
-      calcVisibleCoverDebounceFun: '',
-      // all album dom
-      albums: '',
-      // gallery scroll container dom
-      galleryScrollContainer: ''
+      ]
     }
   },
   methods: {
     /**
-     * @description             init
-     * @return     {undefined}  no return
+     * @description               turn to content album
+     * @return       {undefined}  no return
      */
-    init () {
-      const { contents: { length }, columns, calcVisibleCover, $nextTick } = this
+    turnToAlbum (index) {
+      const { $router, contents } = this
 
-      columns.push(...new Array(length < 4 ? length : 4).fill('').map((t, i) => i))
-
-      $nextTick(calcVisibleCover)
+      $router.push(`/home/gallery/${contents[index].title}`)
     },
-    /**
-     * @description             calc visible cover in gallery scroll container
-     * @return     {undefined}  no return
-     */
-    calcVisibleCover () {
-      if (!this.albums) {
-        this.albums = Array.from(document.getElementsByClassName('album'))
-
-        this.galleryScrollContainer = this.$refs['gallery-scroll-container']
-      }
-
-      const { albums, galleryScrollContainer: { scrollTop, clientHeight: containerHeight } } = this
-
-      albums.forEach(a => {
-        const { offsetTop, clientHeight } = a
-
-        // overfolw
-        if (scrollTop > offsetTop || offsetTop + clientHeight - scrollTop > containerHeight) {
-          a.setAttribute('class', 'album')
-        } else {
-          a.setAttribute('class', 'album current')
-        }
-      })
-    },
-    /**
-     * @description             debounce version of calc visible cover fun
-     * @return     {undefined}  no return
-     */
-    calcVisibleCoverDebounce () {
-      const { calcVisibleCoverDebounceFun, debounce } = this
-
-      if (!calcVisibleCoverDebounceFun) {
-        this.calcVisibleCoverDebounceFun = debounce(100, this.calcVisibleCover)
-
-        this.calcVisibleCoverDebounceFun()
-      } else {
-        calcVisibleCoverDebounceFun()
-      }
-    }
+    ...mapMutations(['setFadeHeader'])
   },
   mounted () {
-    const { init } = this
-
-    init()
   }
 }
 </script>
@@ -149,114 +86,8 @@ export default {
 <style lang="less">
 @import url('../../assets/style/index.less');
 
-// #gallery-contents {
-//   width: 100%;
-//   height: 100%;
-//   display: flex;
-//   justify-content: space-around;
-
-//   .gallery-scroll-container {
-//     display: flex;
-//     flex-direction: row;
-//     box-sizing: border-box;
-//     overflow-y: scroll;
-//     padding: 0 100px;
-
-//     &.less-four {
-//       align-items: center;
-//       padding: 0 100px 100px 100px;
-//     }
-//   }
-
-//   .column {
-//     position: relative;
-//     flex: 1;
-//     margin: 0 10px;
-//     max-width: 300px;
-//     padding: 100px 0px;
-//   }
-
-//   .album {
-//     position: relative;
-//     box-sizing: border-box;
-//     width: 100%;
-//     padding: 10px;
-//     margin-bottom: 10px;
-//     border-radius: 15px;
-//     opacity: 0.15;
-//     transition: all 1s;
-//     overflow: hidden;
-//     cursor: pointer;
-//     .SBS(fade(@BSC, 60));
-
-//     &.current {
-//       opacity: 1;
-//     }
-
-//     &:hover {
-//       .SBS(fade(@BSC, 80));
-//       opacity: 1;
-
-//       img {
-//         opacity: 1;
-//         transform: scale(1.1);
-//       }
-
-//       .info {
-//         border: 3px solid #fff;
-//         transform: translate(-50%, -50%) scale(0.9);
-//         text-shadow: 0 0 0px !important;
-
-//         .info-container {
-//           background-color: @GCIBC;
-//         }
-//       }
-//     }
-
-//     img {
-//       width: 100%;
-//       border-radius: 15px;
-//       transform: scale(1);
-//       transition: all 0.3s;
-//       opacity: 0.8;
-//       cursor: pointer;
-//     }
-
-//     .info {
-//       position: absolute;
-//       top: 50%;
-//       left: 50%;
-//       width: 100%;
-//       height: 100%;
-//       border: 1px solid #fff;
-//       transform: translate(-50%, -50%) scale(1);
-//       transition: all 0.3s;
-//       color: #fff;
-//       .STS(gray);
-
-//       .info-container {
-//         position: absolute;
-//         width: 100%;
-//         bottom: 30px;
-//       }
-
-//       .name, .describe {
-//         text-indent: 40px;
-//       }
-
-//       .name {
-//         height: 40px;
-//         font-size: @GCNFS;
-//         line-height: 40px;
-//         font-weight: bold;
-//       }
-
-//       .describe {
-//         height: 30px;
-//         font-size: @GCDFS;
-//         line-height: 30px;
-//       }
-//     }
-//   }
-// }
+#gallery-contents {
+  width: 100%;
+  height: 100%;
+}
 </style>
