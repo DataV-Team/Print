03aaabbb19ebb743b427676f115e7dccbf22783f
jiaming <743192023@qq.com>
2018-11-29 18:51:47 +0800

    add music audio state

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // BGM play status
+    bgmStatus: true,
+    // music audio for canvas frequency component
     musicAudio: '',
+    // weather fade header
     fadeHeader: false
   },
   mutations: {
+    setBgmStatus (state, status) {
+      state.bgmStatus = status
+    },
     setMusicAudio (state, audio) {
       state.musicAudio = audio
     },
