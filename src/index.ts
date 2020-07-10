db81bbb1e3ab0c4c67ed55403688e46ba1f18468
jiaming743 <743192023@qq.com>
2020-07-10 11:56:41 +0800

    Perfect: You can clear the console before print

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,51 @@
 const print = {
-  log(info: string): void {
+  log(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log(info)
   },
-  warn(info: string): void {
+  warn(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log('\x1b[31;33m' + info + '\x1b[0m')
   },
-  error(info: string): void {
+  error(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log('\x1b[31;40m' + info + '\x1b[0m')
   },
-  success(info: string): void {
+  success(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log('\x1b[42;30m' + info + '\x1b[0m')
   },
-  tip(info: string): void {
+  tip(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log('\x1b[40;32m' + info + '\x1b[0m')
   },
-  red(info: string): void {
+  red(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log('\x1b[31m' + info + '\x1b[39m')
   },
-  green(info: string): void {
+  green(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log('\x1b[32m' + info + '\x1b[39m')
   },
-  white(info: string): void {
+  white(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log('\x1b[37m' + info + '\x1b[39m')
   },
-  yellow(info: string): void {
+  yellow(info: string, clear?: boolean): void {
+    if (clear) print.clear()
+
     console.log('\x1b[31;33m' + info + '\x1b[0m')
+  },
+  clear(): void {
+    process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
   },
 }
 
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,13 @@
 declare const print: {
-    log(info: string): void;
-    warn(info: string): void;
-    error(info: string): void;
-    success(info: string): void;
-    tip(info: string): void;
-    red(info: string): void;
-    green(info: string): void;
-    white(info: string): void;
-    yellow(info: string): void;
+    log(info: string, clear?: boolean | undefined): void;
+    warn(info: string, clear?: boolean | undefined): void;
+    error(info: string, clear?: boolean | undefined): void;
+    success(info: string, clear?: boolean | undefined): void;
+    tip(info: string, clear?: boolean | undefined): void;
+    red(info: string, clear?: boolean | undefined): void;
+    green(info: string, clear?: boolean | undefined): void;
+    white(info: string, clear?: boolean | undefined): void;
+    yellow(info: string, clear?: boolean | undefined): void;
+    clear(): void;
 };
 export default print;
